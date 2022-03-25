a46c4e6c432031e9d8a3cf86ac47c0f90fd6aee1
isranyag777 <israprojects777@gmail.com>
2022-03-25 11:29:01 -0500

    cambio a mysql

--- config/settings.py
+++ config/settings.py
@@ -81,12 +81,23 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': 'usersmng.db',
+#    }
+#}
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'usersmng.db',
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'ivmaster',
+            'USER': 'root',
+            'PASSWORD': 'L1nuxf0r3v3r.',
+            'HOST': 'localhost',
+            'PORT': '3306',
+        }
     }
-}
 
 
 # Password validation
